03c05fa5939df264f1f80bff9d942eb10d10df3f
J. Christian Romero <jcromerohdz@gmail.com>
2022-05-27 12:55:48 -0700

    Adding url_for and base.html jinja2 template in order that the other pages can inherit from it

--- hello.py
+++ hello.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, render_template_string, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-		return '<p>Index Page</p>'
+		return render_template('index.html') 
 
-@app.route('/hello')
-def hello():
-		return '<p>Hello Worlds!!</p>'
+@app.route('/hello/<int:planet>')
+def hello(planet):
+		return render_template('hello.html', t_planet=planet) 
+
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+		if request.method == 'POST':
+				first_name = request.values.get('first_name')
+				last_name = request.values.get('last_name')
+				return redirect(url_for('registered')) 
+		return render_template('form.html')
+
+@app.route('/thank_you')
+def registered():
+		return 'Thank you!'
